c06aac45f7bacd243e7505b6eca8e002ae68de87
Noah Gibbs <noah_gibbs@yahoo.com>
2014-07-08 19:23:25 -0700

    Remove now-unneeded NGinX workaround.

--- site-cookbooks/basic_config/recipes/default.rb
+++ site-cookbooks/basic_config/recipes/default.rb
@@ -38,11 +38,3 @@ template "/home/#{app_user}/.bash_profile" do
   group app_user
   mode "0744"
 end
-
-
-# NginX workaround: http_gzip_static_module needs this directory, and winds up
-# dying because it's not there yet when the nginx::source module includes it.
-directory "/etc/nginx/conf.d" do
-  recursive true
-end
-
