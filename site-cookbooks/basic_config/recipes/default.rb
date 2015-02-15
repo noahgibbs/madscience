c096b4016c59b4835963f5176666a3a2c6bc8e63
Noah Gibbs <noah_gibbs@yahoo.com>
2015-02-15 14:03:25 -0800

    More attempted Postgres workarounds, including doing basic config first.

--- site-cookbooks/basic_config/recipes/default.rb
+++ site-cookbooks/basic_config/recipes/default.rb
@@ -36,3 +36,30 @@ node["users"].each do |app_user, user_data|
     mode "0744"
   end
 end
+
+# This is a workaround for a nasty Chef bug that breaks the Postgres cookbook
+# and must be executed before it.  See COOK-1406, which Chef thinks is fixed.
+# I agree with other commenters that this bug still clearly affects Chef
+# 12.0.3, alas.
+
+fix = Chef::Util::FileEdit.new("/opt/chef/embedded/lib/ruby/2.1.0/x86_64-linux/rbconfig.rb")
+fix.search_file_delete_line("^.*LIBPATHENV.*$")
+fix.write_file
+
+# And here's a workaround because Postgres needs this repo installed and
+# "apt-get update" executed before it runs, but it doesn't call apt-get
+# update. So we need to do it first.
+
+apt_repository 'apt.postgresql.org' do
+  uri 'http://apt.postgresql.org/pub/repos/apt'
+  distribution "#{node['lsb']['codename']}-pgdg"
+  components ['main', '9.3']
+  key 'http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc'
+  action :add
+end
+
+e = execute 'apt-get update' do
+  action :nothing
+end
+
+e.run_action :run
