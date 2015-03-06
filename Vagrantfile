76a2aabe5713339d41761a728a7bc0589f59cf94
Noah Gibbs <noah_gibbs@yahoo.com>
2015-03-05 21:06:54 -0800

    Add production file to replace aws, digital_ocean, etc, and add cap provisioner

--- Vagrantfile
+++ Vagrantfile
@@ -185,4 +185,14 @@ unset -v _ORIGINAL_GEM_PATH GEM_PATH GEM_HOME GEM_ROOT BUNDLE_BIN_PATH BUNDLE_GE
     chef.run_list = run_list
   end
 
+  config.vm.provision :host_shell do |shell|
+    rails_apps = chef_json["ruby_apps"].keys
+    # Combination of clean env, bundle exec and subshell taken from mfenner's vagrant-capistrano-push plugin.
+    # Plus use a login subshell to make sure rvm is all set up.
+    app_lines = rails_apps.map { |app| "echo Deploying #{app}...\nbash -l -c \"INSTALL_APP=#{app} bundle exec cap production deploy\"" }.join("\n")
+    shell.inline = <<-SCRIPT_START + app_lines
+#List of unset variables from Vagrant::Util::Env.with_clean_env
+unset -v _ORIGINAL_GEM_PATH GEM_PATH GEM_HOME GEM_ROOT BUNDLE_BIN_PATH BUNDLE_GEMFILE RUBYLIB RUBYOPT RUBY_ENGINE RUBY_ROOT RUBY_VERSION
+    SCRIPT_START
+  end
 end
---
+++ config/deploy/production.rb
@@ -0,0 +1,82 @@
+# First, get Digital Ocean settings
+
+# Parse output of ssh-config. Example:
+# Host default
+#   HostName 104.131.251.227
+#   User root
+#   Port 22
+#   UserKnownHostsFile /dev/null
+#   StrictHostKeyChecking no
+#   PasswordAuthentication no
+#   IdentityFile /Users/noah/.deploy_credentials/id_rsa_provisioning_4096
+#   IdentitiesOnly yes
+#   LogLevel FATAL
+#   ForwardAgent yes
+
+ssh_conf = `vagrant ssh-config`
+ssh_opts = {}
+ssh_conf.split("\n").map(&:strip).each { |line| key, val = line.split(/\s+/, 2); ssh_opts[key] = val }
+
+home_dir = ENV['HOME'] || ENV['userdir'] || "/home/#{ENV['USER']}"
+creds_dir = File.join home_dir, ".deploy_credentials"
+
+app_user = 'www'  # FIXME
+
+server ssh_opts['HostName'],
+  user: app_user,
+  roles: %w{web app db},
+  ssh_options: {
+    #user: 'user_name', # overrides user setting above
+    keys: [ File.join(creds_dir, "id_rsa_deploy_4096") ],
+    forward_agent: true,
+    auth_methods: %w(publickey),
+    port: ssh_opts['Port'],
+    paranoid: false,  # Unfortunately, real hosting services often reassign IPs
+    # password: 'please use only keys'
+  }
+
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+#role :app, %w{deploy@example.com}
+#role :web, %w{deploy@example.com}
+#role :db,  %w{deploy@example.com}
+
+
+# Extended Server Syntax
+# ======================
+# This can be used to drop a more detailed server definition into the
+# server list. The second argument is a, or duck-types, Hash and is
+# used to set extended properties on the server.
+
+#server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+
+
+# Custom SSH Options
+# ==================
+# You may pass any option but keep in mind that net/ssh understands a
+# limited set of options, consult[net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start).
+#
+# Global options
+# --------------
+#  set :ssh_options, {
+#    keys: %w(/home/rlisowski/.ssh/id_rsa),
+#    forward_agent: false,
+#    auth_methods: %w(password)
+#  }
+#
+# And/or per server (overrides global)
+# ------------------------------------
+# server 'example.com',
+#   user: 'user_name',
+#   roles: %w{web app},
+#   ssh_options: {
+#     user: 'user_name', # overrides user setting above
+#     keys: %w(/home/user_name/.ssh/id_rsa),
+#     forward_agent: false,
+#     auth_methods: %w(publickey password)
+#     # password: 'please use keys'
+#   }
