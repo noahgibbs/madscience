d4f4c75472ad8cbe78d6aaf3692439dd4a7d729d
Noah Gibbs <noah_gibbs@yahoo.com>
2015-03-07 23:38:35 -0800

    Attempt to get it really working with multiple servers

--- config/madscience_config.rb
+++ config/madscience_config.rb
@@ -4,27 +4,35 @@ require "erubis"
 require "json"
 
 def get_chef_json_by_vm
-  json_erb_path = File.join(File.dirname(__FILE__), "..", "nodes", "all_nodes.json.erb")
-  eruby = Erubis::Eruby.new File.read(json_erb_path)
+  json_files = Dir[File.join(File.dirname(__FILE__), "..", "nodes", "*.json.erb")]
+  json_by_vm = {}
 
-  chef_json = JSON.parse eruby.result({})
-  raise "Can't read JSON file for vagrant Chef node!" unless chef_json
+  json_files.each do |file|
+    eruby = Erubis::Eruby.new File.read file
 
-  home_dir = ENV['HOME'] || ENV['userprofile']
-  creds_dir = File.join(home_dir, '.deploy_credentials')
+    chef_json = JSON.parse eruby.result({})
+    raise "Can't read JSON file for MadScience node #{file}!" unless chef_json
 
-  # Read local credentials and pass them to Chef
-  # We need to pass in the private deploy key so that Capistrano can clone your Git repo from the host
-  chef_json['ssh_public_provisioning_key'] = File.read File.join(creds_dir, 'id_rsa_provisioning_4096.pub')
-  chef_json['ssh_public_deploy_key'] = File.read File.join(creds_dir, 'id_rsa_deploy_4096.pub')
-  chef_json['ssh_private_deploy_key'] = File.read File.join(creds_dir, 'id_rsa_deploy_4096')
+    home_dir = ENV['HOME'] || ENV['userprofile']
+    creds_dir = File.join(home_dir, '.deploy_credentials')
 
-  # For authorized keys, let in anybody you specified in ~/.deploy_credentials/authorized_keys, plus the
-  # provisioning and deploy keys.
-  chef_json['authorized_keys'] = [
-    File.read(File.join(creds_dir, 'authorized_keys')),
-    chef_json['ssh_public_deploy_key'],
-    chef_json['ssh_public_provisioning_key'] ].join("\n")
+    # Read local credentials and pass them to Chef
+    # We need to pass in the private deploy key so that Capistrano can clone your Git repo from the host
+    chef_json['ssh_public_provisioning_key'] = File.read File.join(creds_dir, 'id_rsa_provisioning_4096.pub')
+    chef_json['ssh_public_deploy_key'] = File.read File.join(creds_dir, 'id_rsa_deploy_4096.pub')
+    chef_json['ssh_private_deploy_key'] = File.read File.join(creds_dir, 'id_rsa_deploy_4096')
 
-  { "app-server" => chef_json }
+    # For authorized keys, let in anybody you specified in ~/.deploy_credentials/authorized_keys, plus the
+    # provisioning and deploy keys.
+    chef_json['authorized_keys'] = [
+      File.read(File.join(creds_dir, 'authorized_keys')),
+      chef_json['ssh_public_deploy_key'],
+      chef_json['ssh_public_provisioning_key'] ].join("\n")
+
+    # Get VM name from filename
+    vm_name = File.split(file)[-1].split(".")[0].gsub("_", "-")
+    json_by_vm[vm_name] = chef_json
+  end
+
+  json_by_vm
 end
